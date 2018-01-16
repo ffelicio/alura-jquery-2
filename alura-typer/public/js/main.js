b532ec52be5748f4174de391321bdd5164b28b5e
Fernando Felício <fernando_felicio82@yahoo.com.br>
2018-01-16 20:03:12 -0200

    Finalização da aula 1

---
+++ alura-typer/public/js/main.js
@@ -0,0 +1,153 @@
+var campo = $('.campo-digitacao');
+var tempoInicial = $("#tempo-digitacao").text();
+var botaoReiniciar = $("#botao-reiniciar");
+
+$(function() {
+    jogo.atualizarTamanhoFrase();
+    jogo.inputJogador();
+    jogo.inicializarCronometro();
+    botaoReiniciar.click(jogo.reiniciar);
+});
+
+var jogo = {
+    atualizarTamanhoFrase : () => {
+        var frase = $(".frase").text();
+        var numPalavras  = frase.split(" ").length;
+        var tamanhoFrase = $("#tamanho-frase");
+        tamanhoFrase.text(numPalavras);
+    },
+    inputJogador : () => {
+        /**
+         * O evento 'input' mapeia tudo que o usuário está digitando.
+         */
+        campo.on("input", function() {
+            jogo.inicializarContadores();
+
+            jogo.compararTextoDigitado();
+        });
+    },
+    inicializarContadores : () => {
+        var conteudo = campo.val();
+
+        /**
+         * Para contar mais precisamente temos que utilizar uma expressão regular no lugar do espaço vazio,
+         * uma expressão regular que busca qualquer caractere, exceto espaço vazio, essa expressão é mostrada abaixo.
+         *
+         * Agora os espaços não são mais considerados como palavras, mas a contagem sempre mostra a quantidade de palavras mais uma,
+         * para resolver isso vamos subtrair um do length do conteúdo.
+         */
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    },
+    compararTextoDigitado : () => {
+        var frase = $(".frase").text();
+        var digitado = campo.val();
+
+        /**
+         * Para saber se o jogador está certo ou não, vamos pegar apenas a parte inicial da frase que possui a mesma quantidade do valor
+         * digitado.
+         * Isso pode ser feito pela função 'substr' (sub-string):
+         * A função substr devolve uma outra string com o tamanho definido nos parâmetros.
+         * O primeiro parâmetro é o inicio, aqui '0', ou seja, sempre a partir do primeiro char.
+         * O segundo define o fim, que é justamente o tamanho do valor digitado.
+         */
+        /*
+        var comparavel = frase.substr(0 , digitado.length);
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde").removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha").removeClass("borda-verde");
+        }
+        */
+
+        /**
+         * A verificação abaixo faz a mesma ação que foi adicionada acima, mas no campo está sendo utilizado o 'toogleClass'.
+         * A função mencionada recebe um segundo parâmetro para informar se sempre será adicionado (2º parâmetro com o valor 'true'),
+         * ou sempre será removido (2º parâmetro com o valor 'false').
+         */
+        // var ehCorreto = (digitado == comparavel); // Comparação ECMA 5
+
+        var ehCorreto = frase.startsWith(digitado); // Comparação ECMA 6
+
+        campo.toggleClass("borda-verde", ehCorreto);
+        campo.toggleClass("borda-vermelha", !ehCorreto);
+    },
+    inicializarCronometro : () => {
+        var tempoRestante = $("#tempo-digitacao").text();
+
+        /**
+         * Na função abaixo não foi utilizado o evento 'click', porque o mesmo não mapeia o campo caso o usuário acesse o input através
+         * do 'TAB'(tabulação). No caso, optou-se pelo evento 'focus', porque ele é usado para detectar quando o usuário entra em um campo,
+         * não necessariamente digitando.
+         *
+         * No campo está sendo executada a função 'one'. Ela serve para escutar o evento somente uma vez.
+         * A função .one() possuí a mesma sintaxe da função .on().
+         * A função 'on' não seria a melhor alternativa, porque se o campo perdesse o foco e retornasse o foco no mesmo, a função
+         * seria executada a todo momento, podendo acarretar outros bugs.
+         */
+        campo.one("focus", function() {
+            botaoReiniciar.attr("disabled", true);
+
+            /**
+             * A função 'setInterval' é própria do Javascript.
+             * Ela faz com que uma determinada ação (passada como primeiro parâmetro),
+             * seja executada em um intervalo de tempo (passado como segundo parâmetro, no nosso caso, 1 segundo, ou 1000 milissegundos).
+             *
+             * Toda função 'setInterval' *retorna o seu próprio id*.
+             * No caso, iremos utilizar a referência da mesma, para parar a sua execução quando o tempo tiver terminado.
+             */
+            var cronometroID = setInterval(function() {
+                tempoRestante--;
+                $("#tempo-digitacao").text(tempoRestante);
+
+                // Verifica se o tempo acabou para poder desabilitar o textarea.
+                if (tempoRestante < 1) {
+                    /**
+                     * A função abaixo serve para parar a execução da função 'setInterval'.
+                     * Ela recebe como argumento, o id da função 'setInterval'.
+                     */
+                    clearInterval(cronometroID);
+
+                    jogo.finalizar();
+                }
+            }, 1000);
+        });
+    },
+    finalizar : () => {
+        /**
+         * Abaixo é utilizada a função 'attr'. Ela serve para resgatar alguma propriedade ou setar algum valor no campo.
+         * Exemplo de resgate do atributo 'rows': campo.attr("rows");
+         * Exemplo de alteração no atributo 'rows' do textarea: campo.attr("rows", 500);
+         */
+        campo.attr("disabled", true);
+
+        /**
+         * Essa ação de adicionar e remover classes se tornou uma tarefa tão comum, que o jQuery criou uma função
+         * específica para isso, a 'toggleClass'.
+         * Ela funciona da seguinte maneira, se no momento que a função for chamada, o elemento possuir a classe,
+         * ela será removida. Mas se o elemento não possuir a classe, ela será adicionada.
+         */
+        campo.toggleClass('campo-desativado');
+
+        botaoReiniciar.removeAttr('disabled');
+
+        placar.inserir();
+    },
+    reiniciar : () => {
+        campo.attr("disabled", false)
+             .toggleClass('campo-desativado')
+             .removeClass('borda-verde')
+             .removeClass('borda-vermelha')
+             .val('');
+
+        $("#contador-palavras").text("0");
+        $("#contador-caracteres").text("0");
+        $("#tempo-digitacao").text(tempoInicial);
+
+        // Reinicia o cronômetro.
+        jogo.inicializarCronometro();
+    }
+}
---
+++ alura-typer/public/js/placar.js
@@ -0,0 +1,144 @@
+let placar = {
+    inserir : () => {
+        /**
+         * A função .find() recebe como parâmetro seletores CSS, e busca em seus filhos algum elemento que atenda aquela busca.
+         */
+        var corpoTabela = $(".placar").find("tbody");
+        var usuario = "Fernando";
+        var linha = placar.montarLinha(usuario);
+
+        /**
+         * A função .append adiciona a string ou elemento HTML que é passada como parâmetro,
+         * como último filho do elemento em qual ela for chamada.
+         */
+        // corpoTabela.append(linha);
+
+        /**
+         * A função .prepend adiciona a string ou elemento HTML que é passada como parâmetro,
+         * como primeiro filho do elemento em qual ela for chamada.
+         */
+        corpoTabela.prepend(linha);
+
+        // Abre a lista de placares
+        $(".placar").slideDown(500);
+
+        placar.scrollar();
+
+        // placar.remover();
+    },
+    montarLinha : (usuario) => {
+        /**
+         * Realizará a criação dos elementos das linhas da tabela diretamente com jQuery, para poder criar efetivamente
+         * o html, ou seja, guardando na memória para poder utilizar posteriormente.
+         */
+        var linha = $("<tr>");
+        var colunaUsuario = $("<td>").text(usuario);
+        var colunaCaracteres = $("<td>").text($("#contador-caracteres").text());
+        var colunaPalavras = $("<td>").text($("#contador-palavras").text());
+        var colunaData = $("<td>").text(placar.data());
+        var colunaRemover = $("<td>");
+
+        var link = $("<a>").attr("href", "#").addClass("botao-remover");
+        var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+        // Icone dentro do <a>
+        link.append(icone);
+
+        // <a> dentro do <td>
+        colunaRemover.append(link);
+
+        linha.append(colunaUsuario);
+        linha.append(colunaCaracteres);
+        linha.append(colunaPalavras);
+        linha.append(colunaData);
+        linha.append(colunaRemover);
+
+        return linha;
+    },
+    remover : () => {
+        $('.botao-remover').click(function(event) {
+            event.preventDefault();
+
+            /**
+             * Remove a linha da tabela, mas neste caso deverá subir a árvore do DOM para chegar a linha da tabela.
+             * $(this) -> botão de remoção
+             *  .parent() -> td (coluna) que engloba o botão de remoção
+             *  .parent() -> tr (linha) da tabela
+             *  .remove() -> instrução para remover a linha.
+             */
+
+            /**
+             * Remove a linha da tabela.
+             * Neste caso, está sendo utilizada a função .closest. Ela é responsável por buscar o elemento mais próximo do objeto
+             * instanciado, no nosso caso a instrução "$(this)" que se refere ao botão de excluir.
+             * No mesmo pode ser incluídos elemento html, id ou classe.
+             */
+            var linhaPlacar = $(this).closest('tr');
+
+            /**
+             * A função .fadeOut é responsável por remover a opacidade do elemento até um ponto e depois modifica a sua
+             * propriedade "display".
+             * A função .fadeIn é responsável por mostrar o elemento fazendo a transição da opcidade.
+             * Existe também a função fadeToggle(), sendo responsável por alternar entre as 2 funções.
+             * Todas as funções de fade mencionadas, recebe um parâmetro(opcional) que se refere ao tempo de execução da ação.
+             * O tempo é em milisegundos.
+             */
+            linhaPlacar.fadeOut(1000);
+
+            /**
+             * A remoção da linha está envolvida na função setTimeout, para poder fazer a exclusão após a finalização da ação de fade
+             * tratada acima.
+             */
+            setTimeout(() => {
+                linhaPlacar.remove();
+            }, 1000);
+        });
+    },
+    data : () => {
+        var date = new Date();
+        var dia = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate());
+        var mes = date.getMonth() + 1;
+            mes = (mes < 10 ? '0' + mes : mes);
+        var ano = date.getFullYear();
+
+        return (dia + '/' + mes + '/' + ano);
+    },
+    mostrar : () => {
+        /**
+         * A função .slideToggle é um atalho para as funções .slideUp e .slideDown.
+         * Ela retira a necessidade de utilizarmos algum tipo de lógica para poder realizar
+         * a ação para mostrar ou o enconder o elemento.
+         * O parâmetro(opcional), se refere ao tempo de transição. Ele é contado como millisegundos.
+         *
+         * A função .stop também foi incluída, para poder evitar a ação de slide várias vezes quando o usuário clica no botão
+         * repetidamente.
+         * Essa função faz exatamente o que precisamos, a animação que estiver acontecendo no momento é interrompida,
+         * e uma próxima é iniciada.
+         */
+        $('.placar').stop().slideToggle(600);
+    },
+    scrollar : () => {
+        // Nesta função será aplicado o scroll até a lista de placares.
+
+        /**
+         * A função .offset nos dá a posição em que determinado elemento se encontra na página. Esta função nos retorna a distância
+         * que o elemento está do topo e a esquerda da página.
+         * Abaixo está sendo utilizado o .top para poder resgatar a distância do elemento até o topo da página e fazer o scroll. 
+         */
+        var posicaoPlacar = $(".placar").offset().top;
+
+        /**
+         * Abaixo estamos utilizando a função .animate para poder fazer o scroll até a lista de placares.
+         * Ela recebe dois parâmetros, um objeto que contém as propriedades CSS a serem animadas e os seus valores,
+         * e o tempo de duração da animação.
+         * 
+         * Para fazer o scroll está sendo utilizada a propriedade 'scrollTop', ela recebe um valor em pixels,
+         * que representa a posição da nossa página para onde desejamos scrollar.
+         */
+        $('html').animate({
+            scrollTop: posicaoPlacar + "px"
+        }, 1000);
+    }
+}
+
+$('#botao-placar').click(placar.mostrar);
