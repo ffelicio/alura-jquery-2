e7674424c4232cf289074bb16ac1bd2bdfe047ad
Fernando Felício <fernando_felicio82@yahoo.com.br>
2018-01-20 14:08:05 -0200

    Finalização do módulo 4.

--- alura-typer/public/js/frase.js
+++ alura-typer/public/js/frase.js
@@ -33,6 +33,27 @@ var frase = {
             $("#spinner").toggle();
         });
     },
+    buscarPorId : () => {
+        $("#spinner").toggle();
+
+        var dados = {id : $('#frase-id').val()};
+
+        //passando objeto como segundo parametro
+        $.get('http://localhost:3000/frases', dados, (fraseSelecionada) => {
+            var frase = $(".frase");
+            frase.text(fraseSelecionada.texto);
+            jogo.atualizarTamanhoFrase();
+            jogo.atualizarTempoInicial(fraseSelecionada.tempo);
+        }).fail(() => {
+            $("#erro").toggle();
+            setTimeout(function(){
+                $("#erro").toggle();
+            }, 2000);
+        }).always(() => {
+            $("#spinner").toggle();
+        });
+    }
 };
 
 $("#botao-frase").click(frase.aleatoria);
+$("#botao-frase-id").click(frase.buscarPorId);
